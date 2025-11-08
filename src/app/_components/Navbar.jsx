7a7e78621af8ef5375f2d529b6bdf837387512ba
essienjoy <ejoy7602@gmail.com>
2025-11-08 14:38:42 +0100

    chaged landing page thin boorder position

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -6,43 +6,41 @@ import HamburgerMenu from "./HamburgerMenu";
 function Navbar() {
 	return (
 		<header className='bg-secondary-black '>
-			<div className='max-w-[var(--container-max)] mx-auto px-[var(--spacing-main)]'>
-				<div className='py-5 border-b border-solid border-b-primary-white flex justify-between items-center text-primary-white '>
-					<HamburgerMenu />
-					<Logo />
+			<div className='max-w-[var(--container-max)] mx-auto px-[var(--spacing-main)] py-5  flex justify-between items-center text-primary-white '>
+				<HamburgerMenu />
+				<Logo />
 
-					<nav className='hidden md:block'>
-						<ul className='flex items-center gap-5'>
-							<li>
-								<Link className='hover:text-primary-orange' href='/'>
-									HOME
-								</Link>{" "}
-							</li>
-							<li>
-								<Link
-									className='hover:text-primary-orange'
-									href='/category/head-phone'>
-									HEADPHONES
-								</Link>{" "}
-							</li>
-							<li>
-								<Link
-									className='hover:text-primary-orange'
-									href='/category/speaker'>
-									SPEAKER
-								</Link>{" "}
-							</li>
-							<li>
-								<Link
-									className='hover:text-primary-orange'
-									href='/category/ear-phone'>
-									EARPHONES
-								</Link>{" "}
-							</li>
-						</ul>
-					</nav>
-					<CartButton />
-				</div>
+				<nav className='hidden md:block'>
+					<ul className='flex items-center gap-5'>
+						<li>
+							<Link className='hover:text-primary-orange' href='/'>
+								HOME
+							</Link>{" "}
+						</li>
+						<li>
+							<Link
+								className='hover:text-primary-orange'
+								href='/category/head-phone'>
+								HEADPHONES
+							</Link>{" "}
+						</li>
+						<li>
+							<Link
+								className='hover:text-primary-orange'
+								href='/category/speaker'>
+								SPEAKER
+							</Link>{" "}
+						</li>
+						<li>
+							<Link
+								className='hover:text-primary-orange'
+								href='/category/ear-phone'>
+								EARPHONES
+							</Link>{" "}
+						</li>
+					</ul>
+				</nav>
+				<CartButton />
 			</div>
 		</header>
 	);
